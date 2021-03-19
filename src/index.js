21a75ad9c2fca6dc8d4396e409930a41e3c74312
Matheus Moreira <matheustjp111@gmail.com>
2021-03-19 18:48:57 -0300

    Challenge Complete

--- src/index.js
+++ src/index.js
@@ -10,19 +10,69 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user){
+    return response.status(404).json({ error: "User not found." });
+  }
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const countTodos = user.todos.length;
+
+  if (!user.pro && countTodos >= 10) {
+    return response.status(403).json({ error: "Not permitted." });
+  }
+
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user){
+    return response.status(404).json({ error: "User not found." });
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: "Todo ID not valid." });
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "The todo does not belong to that user." });
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user){
+    return response.status(404).json({ error: "User not found." });
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
